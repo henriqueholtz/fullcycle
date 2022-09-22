9203fc2d8a6d1a89d220e0d31772dd98bf0c1f6d
henriqueholtz <henrique_holtz@hotmail.com>
2022-09-21 21:22:52 -0300

    chore: GraphQL => implementing "queryResolvers"

--- fc30/communication-between-systems/GraphQL/graph/resolver.go
+++ fc30/communication-between-systems/GraphQL/graph/resolver.go
@@ -1,7 +1,14 @@
 package graph
 
+import "github.com/henriqueholtz/fullcycle/fc30/communication-between-systems/GraphQL/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	Categories []*model.Category
+	Courses []*model.Course
+	Chapters []*model.Chapter
+
+}
