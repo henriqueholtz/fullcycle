5b79fa59918c7ffc4a12d5cc987bf2a56c0cc1d3
henriqueholtz <henrique_holtz@hotmail.com>
2022-10-21 07:19:15 -0300

    chore: DDD => Creating unit tests to customer

--- fc30/domain-driven-design/src/entity/customer.spec.ts
+++ fc30/domain-driven-design/src/entity/customer.spec.ts
@@ -1,6 +1,43 @@
+import Address from './address';
+import Customer from './customer';
+
 describe('Customer unit tests', () => {
-  it('Should get 1 as result', () => {
-    const result = 1;
-    expect(result).toBe(1);
+  const address = new Address('street', '10A', 'New York');
+  it('Should throw error when id is empty', () => {
+    expect(() => {
+      let customer = new Customer('', 'Jhon', address);
+    }).toThrowError('Id is required!');
+  });
+
+  it('Should throw error when id is empty', () => {
+    expect(() => {
+      let customer = new Customer('123', '', address);
+    }).toThrowError('Name is required!');
+  });
+
+  it('Should change name correctly', () => {
+    const newName: string = 'Jane';
+    let customer = new Customer('123', 'Jhon', address);
+    customer.changeName(newName);
+    expect(customer.name).toBe(newName);
+  });
+
+  it('Should activate customer', () => {
+    let customer = new Customer('123', 'Jhon', address);
+    customer.activate();
+    expect(customer.isActive()).toBe(true);
+  });
+
+  it('Should deactivate customer', () => {
+    let customer = new Customer('123', 'Jhon', address);
+    customer.deactivate();
+    expect(customer.isActive()).toBe(false);
+  });
+
+  it('Should throw error when Address is undefined when activating a customer', () => {
+    expect(() => {
+      let customer = new Customer('123', 'Jhon', undefined);
+      customer.activate();
+    }).toThrowError('Address is required to activate the customer!');
   });
 });
--- fc30/domain-driven-design/src/entity/customer.ts
+++ fc30/domain-driven-design/src/entity/customer.ts
@@ -1,12 +1,12 @@
 import Address from './address';
 
 export default class Customer {
-  _id: string;
-  _name: string;
-  _address: Address;
-  _active: boolean = true;
+  private _id: string;
+  private _name: string;
+  private _address: Address | undefined;
+  private _active: boolean = false;
 
-  constructor(id: string, name: string, address: Address) {
+  constructor(id: string, name: string, address: Address | undefined) {
     this._id = id;
     this._name = name;
     this._address = address;
@@ -14,10 +14,27 @@ export default class Customer {
   }
 
   validate() {
-    if (this._name.length === 0) throw new Error('Name is required');
+    if (this._id.length === 0) throw new Error('Id is required!');
+    if (this._name.length === 0) throw new Error('Name is required!');
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  isActive(): boolean {
+    return this._active;
   }
 
   activate() {
+    if (this._address === undefined)
+      throw new Error('Address is required to activate the customer!');
+
     this._active = true;
   }
 
