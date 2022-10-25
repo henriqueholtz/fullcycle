ada69b2ac91db1daef7ba2bf1c6ca5ea64f0e2d8
henriqueholtz <henrique_holtz@hotmail.com>
2022-10-25 20:40:32 -0300

    chore: DDD => check build before run unit tests (using "tsc --noEmit"), and config to run it on jestRunner extension

--- fc30/domain-driven-design/src/main.ts
+++ fc30/domain-driven-design/src/main.ts
@@ -6,8 +6,8 @@ import Order from './entity/order';
 let address = new Address('Street Abc', '10A', 'New York');
 let customer = new Customer('123', 'Henrique Holtz', address);
 
-const item1 = new OrderItem('1', 'Item 1', 11);
-const item2 = new OrderItem('2', 'Item 2', 22);
-const item3 = new OrderItem('3', 'Item 3', 33);
+const item1 = new OrderItem('1', 'Item 1', 11, 'p1', 1);
+const item2 = new OrderItem('2', 'Item 2', 22, 'p2', 2);
+const item3 = new OrderItem('3', 'Item 3', 33, 'p3', 3);
 
 const order = new Order('1', '123', [item1, item2, item3]);
