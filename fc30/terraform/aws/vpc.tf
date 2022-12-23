e46268295f588a4038f7257792cae56f5d7d11d0
henriqueholtz <henrique_holtz@hotmail.com>
2022-12-23 07:15:37 -0300

    chore: Terraform => Add aws_subnet dynamicly

--- fc30/terraform/aws/vpc.tf
+++ fc30/terraform/aws/vpc.tf
@@ -11,20 +11,31 @@ output "az" {
   value = "${data.aws_availability_zones.available.names}"
 }
 
-resource "aws_subnet" "new-subnet-1" {
+resource "aws_subnet" "subnets" {
+  count = 2
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   vpc_id = aws_vpc.new-vpc.id
-  cidr_block = "10.0.0.0/24"  
-  availability_zone = "us-east-1a"
+  cidr_block = "10.0.${count.index}.0/24"  
+  map_public_ip_on_launch = true
   tags = {
-    "Name" = "${var.prefix}-subnet-1"
+    "Name" = "${var.prefix}-subnet-${count.index}"
   }
 }
 
-resource "aws_subnet" "new-subnet-2" {
-  vpc_id = aws_vpc.new-vpc.id
-  cidr_block = "10.0.1.0/24"  
-  availability_zone = "us-east-1b"
-  tags = {
-    "Name" = "${var.prefix}-subnet-2"
-  }
-}
+# resource "aws_subnet" "new-subnet-1" {
+#   vpc_id = aws_vpc.new-vpc.id
+#   cidr_block = "10.0.0.0/24"  
+#   availability_zone = "us-east-1a"
+#   tags = {
+#     "Name" = "${var.prefix}-subnet-1"
+#   }
+# }
+
+# resource "aws_subnet" "new-subnet-2" {
+#   vpc_id = aws_vpc.new-vpc.id
+#   cidr_block = "10.0.1.0/24"  
+#   availability_zone = "us-east-1b"
+#   tags = {
+#     "Name" = "${var.prefix}-subnet-2"
+#   }
+# }
