764842c6bd373bf0ec3cb28b45a6154d06f47a64
henriqueholtz <henrique_holtz@hotmail.com>
2022-12-24 06:27:03 -0300

    chore: Terraform => Add IAM roles with policies

--- fc30/terraform/aws/cluster.tf
+++ fc30/terraform/aws/cluster.tf
@@ -13,3 +13,31 @@ resource "aws_security_group" "sec-group" {
         Name = "${var.prefix}-sec-group"
     }
 }
+
+resource "aws_iam_role" "cluster-role" {
+    name = "${var.prefix}-${var.cluster_name}"
+    assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}   
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role = aws_iam_role.cluster-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  role = aws_iam_role.cluster-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
