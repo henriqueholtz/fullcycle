38ed1223083d19afc1d594eeb8297ee7e39736ac
henriqueholtz <henrique_holtz@hotmail.com>
2022-12-31 07:13:28 -0300

    chore: Terraform => Moving cluster eks as a module

--- fc30/terraform/aws/modules/eks/main.tf
+++ fc30/terraform/aws/modules/eks/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "sec-group" {
-    vpc_id = aws_vpc.new-vpc.id
+    vpc_id = var.vpc_id
 
     egress {
         from_port = 0
@@ -52,12 +52,63 @@ resource "aws_eks_cluster" "cluster" {
   role_arn = aws_iam_role.cluster-role.arn
   enabled_cluster_log_types = ["api","audit"]
   vpc_config {
-      subnet_ids = aws_subnet.subnets[*].id # var.subnet_ids 
+      subnet_ids = var.subnet_ids 
       security_group_ids = [aws_security_group.sec-group.id]
   }
   depends_on = [
     aws_cloudwatch_log_group.log,
     aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,
     aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
+  ]
+}
+resource "aws_iam_role" "node-role" {
+    name = "${var.prefix}-${var.cluster_name}-node-role"
+    assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_eks_node_group" "node-1" {
+  cluster_name = aws_eks_cluster.cluster.name
+  node_group_name = "node-1"
+  node_role_arn = aws_iam_role.node-role.arn
+  subnet_ids = var.subnet_ids
+  instance_types = ["t3.micro"]
+  scaling_config {
+    desired_size = var.desired_size
+    max_size = var.max_size
+    min_size = var.min_size
+  }
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
--- fc30/terraform/aws/terraform.tfvars
+++ fc30/terraform/aws/terraform.tfvars
@@ -4,3 +4,4 @@ retention_in_days = 3
 desired_size = 2
 max_size = 3
 min_size = 1
+vpc_cidr_block = "10.0.0.0/16"
