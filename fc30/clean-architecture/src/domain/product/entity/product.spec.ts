1342e349adbf35dfc72f568d5a52f3971101e4c7
henriqueholtz <henrique_holtz@hotmail.com>
2023-08-18 07:52:58 -0300

    feat: Clean Code => Implementing notificationerrors to product entity with unit tests

--- fc30/clean-architecture/src/domain/product/entity/product.spec.ts
+++ fc30/clean-architecture/src/domain/product/entity/product.spec.ts
@@ -1,22 +1,30 @@
 import Product from './product';
 
 describe('Product unit tests', () => {
+  it('Should throw error when id and name are empty and price is invalid', () => {
+    expect(() => {
+      let product = new Product('', '', -100);
+    }).toThrowError(
+      'product: Id is required!,product: Name is required!,product: Price must be grater than zero!'
+    );
+  });
+
   it('Should throw error when id is empty', () => {
     expect(() => {
       let product = new Product('', 'Product 1', 100);
-    }).toThrowError('Id is required!');
+    }).toThrowError('product: Id is required!');
   });
 
   it('Should throw error when name is empty', () => {
     expect(() => {
       let product = new Product('1', '', 100);
-    }).toThrowError('Name is required!');
+    }).toThrowError('product: Name is required!');
   });
 
   it('Should throw error when price is less than zero', () => {
     expect(() => {
       let product = new Product('1', 'Product 1', -2);
-    }).toThrowError('Price must be grater than zero!');
+    }).toThrowError('product: Price must be grater than zero!');
   });
 
   it('Should change name', () => {
