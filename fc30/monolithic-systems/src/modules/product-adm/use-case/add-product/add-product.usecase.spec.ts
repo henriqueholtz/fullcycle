04e5d2ea0a2db8f797586c52658ebe6da47c9d1c
henriqueholtz <henrique_holtz@hotmail.com>
2023-08-28 07:46:51 -0300

    feat: Monolithic systems => product-adm: Creating uscase 'add-product'

---
+++ fc30/monolithic-systems/src/modules/product-adm/use-case/add-product/add-product.usecase.spec.ts
@@ -0,0 +1,32 @@
+import { AddProductInputDto, AddProductOutputDto } from './add-product.dto';
+import AddProductUseCase from './add-product.usecase';
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
+
+describe('Add Product usecase unit test', () => {
+  it('should add a product', async () => {
+    const productRepository = MockRepository();
+    const usecase = new AddProductUseCase(productRepository);
+
+    const input: AddProductInputDto = {
+      name: 'Product 1',
+      description: 'Product 1 description',
+      purchasePrice: 100,
+      stock: 10,
+    };
+
+    const result: AddProductOutputDto = await usecase.execute(input);
+
+    expect(productRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined;
+    expect(result.name).toBe(input.name);
+    expect(result.description).toBe(input.description);
+    expect(result.purchasePrice).toBe(input.purchasePrice);
+    expect(result.stock).toBe(input.stock);
+  });
+});
