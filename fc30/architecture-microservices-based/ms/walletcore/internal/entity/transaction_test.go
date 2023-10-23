8ff137528ac80c3e9b830967a797e1939717ea9c
henriqueholtz <henrique_holtz@hotmail.com>
2023-10-23 07:59:45 -0300

    feat: Architecture Microservices => Creating Transaction entity with unit tests

---
+++ fc30/architecture-microservices-based/ms/walletcore/internal/entity/transaction_test.go
@@ -0,0 +1,46 @@
+package entity 
+
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestCreateTransactionSuccessfully(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+
+	client2, _ := NewClient("John Doe 2", "j2@j.com")
+	account2 := NewAccount(client2)
+
+	account.Credit(1000)
+	account2.Credit(500)
+	
+	transaction, err := NewTransaction(account, account2, 150)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, transaction)
+	assert.Equal(t, account.Balance, 850)
+	assert.Equal(t, account2.Balance, 650)
+}
+
+
+func TestCreateTransactionWithoutSufficientFunds(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+
+	client2, _ := NewClient("John Doe 2", "j2@j.com")
+	account2 := NewAccount(client2)
+
+	account.Credit(1000)
+	account2.Credit(500)
+	
+	transaction, err := NewTransaction(account, account2, 1500)
+
+	assert.NotNil(t, err)
+	assert.Error(t, err, "Insufficient funds!")
+	assert.Nil(t, transaction)
+	assert.Equal(t, account.Balance, 1000)
+	assert.Equal(t, account2.Balance, 500)
+}
