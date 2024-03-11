432345540ed933b42f2b6bc84de5d5bc4fff031e
Henrique Holtz <henrique_holtz@hotmail.com>
2024-03-11 07:17:08 -0300

    feat: EDA => Creating a "kafka-handler" for "TransactionCreated" event

---
+++ fc30/event-driven-architecture/internal/event/handler/transaction_created_kafka.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/henriqueholtz/fullcycle/fc30/event-driven-architecture/pkg/events"
+	"github.com/henriqueholtz/fullcycle/fc30/event-driven-architecture/pkg/kafka"
+)
+
+type TransactionCreatedKafkaHandler struct {
+	Kafka *kafka.Producer
+}
+
+func NewTransactionCreatedKafkaHandler(kafka *kafka.Producer) *TransactionCreatedKafkaHandler {
+	return &TransactionCreatedKafkaHandler{
+		Kafka: kafka,
+	}
+}
+
+func (h *TransactionCreatedKafkaHandler) Handle(message events.IEvent, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	h.Kafka.Publish(message, nil, "transactions")
+	fmt.Println("TransactionCreatedKafkaHandler: ", message)
+}
