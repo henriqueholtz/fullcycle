d08c9935cd7de6dfad65a1ef94c04d2c2aab3549
Henrique Holtz <henrique_holtz@hotmail.com>
2024-03-16 13:45:14 -0300

    feat: EDA => Consuming kafka event from topic "balances" to update accounts' balances

--- fc30/event-driven-architecture/cmd/wallet-reflected/main.go
+++ fc30/event-driven-architecture/cmd/wallet-reflected/main.go
@@ -17,16 +17,29 @@ import (
 	"github.com/henriqueholtz/fullcycle/fc30/event-driven-architecture/internal/entity"
 	"github.com/henriqueholtz/fullcycle/fc30/event-driven-architecture/pkg/uow"
 )
-type KafkaMessage struct {
+
+type KafkaMessageTransaction struct {
 	Name string
-	Payload KafkaEventPayloadDTO
+	Payload KafkaEventTransactionPayloadDTO
 }
 
-type KafkaEventPayloadDTO struct {
+type KafkaEventTransactionPayloadDTO struct {
 	ClientId string `json:"id"`
 	AccountIDFrom string `json:"account_id_from"`
 	AccountIDTo string `json:"account_id_to"`
 	Amount float64 `json:"amount"`
+}
+
+type KafkaMessageBalance struct {
+	Name string
+	Payload KafkaEventBalancePayloadDTO
+}
+
+type KafkaEventBalancePayloadDTO struct {
+	AccountIDFrom string `json:"account_id_from"`
+	AccountIDTo string `json:"account_id_to"`
+	BalanceAccountIdFrom float64 `json:"balance_account_id_from"`
+	BalanceAccountIdTo float64 `json:"balance_account_id_to"`
 }
 
 func main() {
@@ -86,8 +99,6 @@ func webServer(db *sql.DB, wg *sync.WaitGroup) {
 	fmt.Println("Server is running at 3003 port")
 }
 
-// TODO: Listen balances to update accounts rows
-
 func kafkaConsumer(db *sql.DB, wg *sync.WaitGroup) {
 	defer wg.Done()
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
@@ -100,7 +111,7 @@ func kafkaConsumer(db *sql.DB, wg *sync.WaitGroup) {
 	}
 
 	fmt.Println("Subscribing kafka topics...")
-	c.SubscribeTopics([]string{"transactions"}, nil)
+	c.SubscribeTopics([]string{"transactions", "balances"}, nil)
 	accountDb := database.NewAccountDB(db)
 	transactionDb := database.NewTransactionDB(db)
 	run := true
@@ -111,45 +122,76 @@ func kafkaConsumer(db *sql.DB, wg *sync.WaitGroup) {
 
 		if err == nil {
 			fmt.Println("Message has been readed...")
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-			kafkaMessage := &KafkaMessage{}
-			errJson := json.Unmarshal(msg.Value, kafkaMessage)
-			if errJson != nil {
-				panic(errJson)
-			}
+			fmt.Printf("Message on %s: %s\n", *msg.TopicPartition.Topic, string(msg.Value))
+			if (*msg.TopicPartition.Topic == "transactions") {
+				kafkaMessage := &KafkaMessageTransaction{}
+				errJson := json.Unmarshal(msg.Value, kafkaMessage)
 
-			fmt.Printf("Amount: %f; ClientId: %s; AccountIDFrom: %s; AccountIDTo: %s\n", kafkaMessage.Payload.Amount, kafkaMessage.Payload.ClientId, kafkaMessage.Payload.AccountIDFrom, kafkaMessage.Payload.AccountIDTo)
-			
-			accountFrom, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDFrom)
-			if dbErr != nil {
-				panic(dbErr)
-			}
-			if accountFrom == nil {
-				panic("AccountFrom does not exist!")
-			}
+				if errJson != nil {
+					panic(errJson)
+				}
+				
+				accountFrom, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDFrom)
+				if dbErr != nil {
+					panic(dbErr)
+				}
+				if accountFrom == nil {
+					panic("AccountFrom does not exist!")
+				}
 
-			accountTo, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDTo)
-			if dbErr != nil {
-				panic(dbErr)
-			}
-			if accountTo == nil {
-				panic("AccountTo does not exist!")
-			}
+				accountTo, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDTo)
+				if dbErr != nil {
+					panic(dbErr)
+				}
+				if accountTo == nil {
+					panic("AccountTo does not exist!")
+				}
 
-			fmt.Printf("Accounts from and to ok...")
-			fmt.Printf("Transaction Amount: %f\n", kafkaMessage.Payload.Amount)
-			transaction, transactionErr := entity.NewTransaction(accountFrom, accountTo, kafkaMessage.Payload.Amount)
-			if transactionErr != nil {
-				panic(transactionErr)
-			}
+				fmt.Printf("[transactions] Accounts from and to ok...")
+				fmt.Printf("Transaction Amount: %f\n", kafkaMessage.Payload.Amount)
+				transaction, transactionErr := entity.NewTransaction(accountFrom, accountTo, kafkaMessage.Payload.Amount)
+				if transactionErr != nil {
+					panic(transactionErr)
+				}
 
-			transactionErr = transactionDb.Create(transaction)
-			if transactionErr != nil {
-				panic(transactionErr)
-			}
-			fmt.Println("Successfuly completed...")
-			fmt.Println("-------------")
+				transactionErr = transactionDb.Create(transaction)
+				if transactionErr != nil {
+					panic(transactionErr)
+				}
+				fmt.Println("[transactions] Successfuly completed...")
+				fmt.Println("[transactions] -------------")
+			} else if (*msg.TopicPartition.Topic == "balances") {
+				
+				kafkaMessage := &KafkaMessageBalance{}
+				errJson := json.Unmarshal(msg.Value, kafkaMessage)
+				if errJson != nil {
+					panic(errJson)
+				}
+				accountFrom, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDFrom)
+				if dbErr != nil {
+					panic(dbErr)
+				}
+				if accountFrom == nil {
+					panic("AccountFrom does not exist!")
+				}
 
+				accountTo, dbErr := accountDb.FindByID(kafkaMessage.Payload.AccountIDTo)
+				if dbErr != nil {
+					panic(dbErr)
+				}
+				if accountTo == nil {
+					panic("AccountTo does not exist!")
+				}
+				fmt.Printf("[balances] Accounts from and to ok...")
+
+				accountFrom.Balance = kafkaMessage.Payload.BalanceAccountIdFrom
+				accountDb.UpdateBalance(accountFrom)
+
+				accountTo.Balance = kafkaMessage.Payload.BalanceAccountIdTo
+				accountDb.UpdateBalance(accountTo)
+				fmt.Println("[balances] Successfuly completed...")
+				fmt.Println("[balances] -------------")
+			}
 		} else if !err.(kafka.Error).IsTimeout() {
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
 			fmt.Println("-------------")
