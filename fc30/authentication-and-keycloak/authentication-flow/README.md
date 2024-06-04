1d4447c9f010f0192f9f9bd0473f8fe805863b8f
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-04 07:19:27 -0300

    feat: Keycloak ("authorization-code") => Adding the "/login" endpoint which redirects to keycloak

--- fc30/authentication-and-keycloak/authentication-flow/README.md
+++ fc30/authentication-and-keycloak/authentication-flow/README.md
@@ -1,5 +1,14 @@
 # Authentication Flows
 
+## Prerequisites
+
+- Create a client on keycloak:
+    - `Client ID`: `fullcycle_client`
+    - `Client authentication` and `Authorization` should be turned off
+    - `Authentication flow`: Check only `Standard flow` and `Direct access grants`
+    - `Root URL`: `http://localhost:3000`
+    - `Valid redirect URIs` and `Web origins`: `/*`
+
 ### Commands
 
 - `docker compose exec app bash`
--- fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
+++ fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
     res.send('Hi there!')
 })
 
+app.get('/login', (req, res) => {
+    const loginParams = new URLSearchParams({
+        client_id: 'fullcycle_client',
+        redirect_uri: 'http://localhost:3000/callback',
+        response_type: 'code',
+        scope: 'openid'
+    })
+    const realm = 'master'
+    const url = `http://localhost:8081/realms/${realm}/protocol/openid-connect/auth?${loginParams.toString()}`
+    
+    res.redirect(url)
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
