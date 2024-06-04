53ee0541f92d40ebb5fb2e599d59e1ed8bfa116f
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-04 07:49:07 -0300

    feat: Keycloak ("authorization-code") => Adding "/callback-login"

--- fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
+++ fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
@@ -1,19 +1,48 @@
 import express from 'express'
 
 const app = express()
+const realm = 'master'
+const client_id = 'fullcycle_client'
+const redirect_uri = 'http://localhost:3000/callback-login'
 
 app.get('/', (req, res) => {
-    res.send('Hi there!')
+    res.send("Hi there! You're in the authorization-code flow.")
 })
+
+app.get('/callback-login', async (req, res) => {
+    console.log(req.query)
+
+    const bodyParams = new URLSearchParams({
+        client_id,
+        grant_type: 'authorization_code',
+        code: req.query.code as string,
+        redirect_uri
+    })
+    const host = 'localhost' //'keycloak' // host.docker.internal
+    const url = `http://${host}:8081/realms/${realm}/protocol/openid-connect/token`
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: bodyParams.toString()
+    })
+    
+    const json = await response.json()
+    console.log(json)
+
+    res.send(json)
+})
+
 
 app.get('/login', (req, res) => {
     const loginParams = new URLSearchParams({
-        client_id: 'fullcycle_client',
-        redirect_uri: 'http://localhost:3000/callback',
+        client_id,
+        redirect_uri,
         response_type: 'code',
         scope: 'openid'
     })
-    const realm = 'master'
     const url = `http://localhost:8081/realms/${realm}/protocol/openid-connect/auth?${loginParams.toString()}`
     
     res.redirect(url)
