5945e0ffbab32e8d2b2c3b7c623661b3964d7447
Henrique Holtz <henrique_holtz@hotmail.com>
2024-06-05 06:41:18 -0300

    feat: Keycloak ("authorization-code") => Adding session and nonce at "/login"

--- fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
+++ fc30/authentication-and-keycloak/authentication-flow/authorization-code-flow/src/index.ts
@@ -1,15 +1,42 @@
 import express from 'express'
+import session from "express-session";
+import crypto from 'crypto'
 
-const app = express()
 const realm = 'master'
 const client_id = 'fullcycle_client'
 const redirect_uri = 'http://localhost:3000/callback-login'
+
+const app = express()
+
+const memoryStore = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: memoryStore,
+    //expires
+  })
+);
 
 app.get('/', (req, res) => {
     res.send("Hi there! You're in the authorization-code flow.")
 })
 
 app.get('/callback-login', async (req, res) => {
+    
+    //@ts-expect-error - type mismatch
+    if (req.session.user) {
+        return res.redirect("/admin");
+    }
+
+    //@ts-expect-error - type mismatch
+    if(req.query.state !== req.session.state) {
+      //poderia redirecionar para o login em vez de mostrar o erro
+      return res.status(401).json({ message: "Unauthenticated" });
+    }
+  
     console.log(req.query)
 
     const bodyParams = new URLSearchParams({
@@ -37,14 +64,22 @@ app.get('/callback-login', async (req, res) => {
 
 
 app.get('/login', (req, res) => {
+
+    const nonce = crypto.randomBytes(16).toString('base64')
+
+    //@ts-expect-error - type mismatch
+    req.session.nonce = nonce;
+    req.session.save();
+
     const loginParams = new URLSearchParams({
         client_id,
         redirect_uri,
         response_type: 'code',
-        scope: 'openid'
+        scope: 'openid',
+        nonce
     })
     const url = `http://localhost:8081/realms/${realm}/protocol/openid-connect/auth?${loginParams.toString()}`
-    
+    console.log(url)
     res.redirect(url)
 })
 
