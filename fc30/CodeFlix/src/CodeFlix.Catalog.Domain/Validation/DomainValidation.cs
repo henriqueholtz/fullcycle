30e746a82d8a39a14494ef803f9f4c4e515a4c69
Henrique Holtz <henrique_holtz@hotmail.com>
2024-07-13 10:20:50 -0300

    feat: Creating DomainValidation with method "NotNull" and "NotNullOrEmpty" and its unit tests

---
+++ fc30/CodeFlix/src/CodeFlix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,17 @@
+﻿using CodeFlix.Catalog.Domain.Exceptions;
+
+namespace CodeFlix.Catalog.Domain.Validation;
+
+public class DomainValidation
+{
+    public static void NotNull(object? target, string fieldName)
+    {
+        if (target is null)
+            throw new EntityValidationException($"{fieldName} should not be null");
+    }
+    public static void NotNullOrEmpty(string? target, string fieldName)
+    {
+        if (string.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException($"{fieldName} should not be null or empty");
+    }
+}
---
+++ fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,71 @@
+﻿using Bogus;
+using CodeFlix.Catalog.Domain.Exceptions;
+using CodeFlix.Catalog.Domain.Validation;
+
+namespace CodeFlix.Catalog.UnitTests.Domain.Validation;
+
+public class DomainValidationTest
+{
+    private Faker _faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        // Arranje
+        var value = _faker.Commerce.ProductName();
+
+        // Act
+        Action action = () => DomainValidation.NotNull(value, "Value");
+
+        // Assert
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        // Arranje
+        string? value = null;
+
+        // Act
+        Action action = () => DomainValidation.NotNull(value, "FieldName");
+
+        // Assert
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+    [Theory(DisplayName = nameof(NotEmptyOrNullThrowWhenEmpty))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public void NotEmptyOrNullThrowWhenEmpty(string? target)
+    {
+        // Arranje
+        string? value = target;
+
+        // Act
+        Action action = () => DomainValidation.NotNullOrEmpty(value, "FieldName");
+
+        // Assert
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null or empty");
+    }
+
+    [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOrEmptyOk()
+    {
+        // Arranje
+        var value = _faker.Commerce.ProductName();
+
+        // Act
+        Action action = () => DomainValidation.NotNullOrEmpty(value, "Value");
+
+        // Assert
+        action.Should().NotThrow();
+    }
+}
