f42ac3189d97ec523dc73d6b4059a539e8ab55aa
Henrique Holtz <henrique_holtz@hotmail.com>
2024-08-07 13:52:46 -0300

    test: CodeFlix => Adding unti test to make sure a "NotFoundException" will be thrown when a category doesn't exists

--- fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Application/GetCategory/GetCategoryTests.cs
+++ fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Application/GetCategory/GetCategoryTests.cs
@@ -1,5 +1,7 @@
 ﻿using CodeFlix.Catalog.UnitTests.Application.GetCategory;
 using UseCaseGetCategory = CodeFlix.Catalog.Application.UseCases.Category.GetCategory;
+using UseCaseCommon = CodeFlix.Catalog.Application.UseCases.Category.Common;
+using CodeFlix.Catalog.Application.Exceptions;
 
 namespace CodeFlix.Catalog.UnitTests;
 
@@ -13,7 +15,7 @@ public class GetCategoryTests
         _fixture = fixture;
     }
 
-    [Fact(DisplayName = "")]
+    [Fact(DisplayName = nameof(GetSuccessfully))]
     [Trait("Application", "GetCategory - Use Cases")]
     public async Task GetSuccessfully() {
         // Arranje
@@ -24,7 +26,7 @@ public class GetCategoryTests
         var useCase = new UseCaseGetCategory.GetCategory(repositoryMock.Object);
 
         // Act
-        UseCaseGetCategory.GetCategoryOutput output = await useCase.Handle(input, CancellationToken.None);
+        UseCaseCommon.CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
 
         // Assert
         output.Should().NotBeNull();
@@ -33,6 +35,26 @@ public class GetCategoryTests
         output.Description.Should().Be(exampleCategory.Description);
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+
+        repositoryMock.Verify(rep => rep.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.VerifyNoOtherCalls();
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesNotExists))]
+    [Trait("Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesNotExists() {
+        // Arranje
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var exampleGuid = Guid.NewGuid();
+        repositoryMock.Setup(r => r.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ThrowsAsync(new NotFoundException($"Category '{exampleGuid}' not found"));
+        var input = new UseCaseGetCategory.GetCategoryInput(exampleGuid);
+        var useCase = new UseCaseGetCategory.GetCategory(repositoryMock.Object);
+
+        // Act
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        // Assert
+        await task.Should().ThrowAsync<NotFoundException>();
 
         repositoryMock.Verify(rep => rep.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
         repositoryMock.VerifyNoOtherCalls();
