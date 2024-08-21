15da1c098459b3bf2e0256d9723841602184e0d9
Henrique Holtz <henrique_holtz@hotmail.com>
2024-08-21 07:58:03 -0300

    test: CodeFlix => Creating a unit test to validate inputs (ListCategoriesInput) with no all parameters (use case to list categories)

--- fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTests.cs
+++ fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTests.cs
@@ -1,3 +1,4 @@
+using CodeFlix.Catalog.Application.UseCases.Category.ListCategories;
 using CodeFlix.Catalog.Domain.Entity;
 using CodeFlix.Catalog.Domain.SeedWork.SearchableRepository;
 using UseCaseListCategories = CodeFlix.Catalog.Application.UseCases.Category.ListCategories;
@@ -19,6 +20,60 @@ public class ListCategoriesTests
     public async Task ListSuccess() {
         var repositoryMock = _fixture.GetRepositoryMock();
         var input = _fixture.GetValidInput();
+        var categories = _fixture.GetCategories();
+        var repositorySearchOutput = new SearchOutput<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            items: categories,
+            total: categories.Count
+        );
+        repositoryMock.Setup(x => x.SearchAsync(
+            It.Is<SearchInput>(i => 
+                i.Page == input.Page
+                && i.PerPage == input.PerPage
+                && i.Search == input.Search
+                && i.OrderBy == input.Sort
+                && i.Order == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(repositorySearchOutput);
+        var useCase = new UseCaseListCategories.ListCategories(repositoryMock.Object);
+
+        // Act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        // Assert
+        output.Should().NotBeNull();
+        output.Page.Should().Be(repositorySearchOutput.CurrentPage);
+        output.PerPage.Should().Be(repositorySearchOutput.PerPage);
+        output.Total.Should().Be(repositorySearchOutput.Total);
+        output.Items.Should().HaveCount(repositorySearchOutput.Items.Count);
+        output.Items.ToList().ForEach(outputItem => {
+            var repositoryCategory = repositorySearchOutput.Items.FirstOrDefault(x => x.Id == outputItem.Id);
+            outputItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(repositoryCategory!.Name);
+            outputItem.Description.Should().Be(repositoryCategory.Description);
+            outputItem.IsActive.Should().Be(repositoryCategory.IsActive);
+            outputItem.CreatedAt.Should().Be(repositoryCategory.CreatedAt);
+        });
+        repositoryMock.Verify(x => x.SearchAsync(
+            It.Is<SearchInput>(p => 
+                p.Page == input.Page
+                && p.PerPage == input.PerPage
+                && p.Search == input.Search
+                && p.OrderBy == input.Sort
+                && p.Order == input.Dir),
+            It.IsAny<CancellationToken>()
+            ), Times.Once
+        );
+        repositoryMock.VerifyNoOtherCalls();
+    }
+
+    [Theory(DisplayName = nameof(ListInputWithNoParametersShouldBeSuccessful))]
+    [Trait("Application", "ListCategories - Use Cases")]
+    [MemberData(nameof(ListCategoriesTestsDataGenerator.GetInputsWithoutAllParameters), parameters: 12, MemberType = typeof(ListCategoriesTestsDataGenerator))]
+    public async Task ListInputWithNoParametersShouldBeSuccessful(ListCategoriesInput input) {
+        var repositoryMock = _fixture.GetRepositoryMock();
         var categories = _fixture.GetCategories();
         var repositorySearchOutput = new SearchOutput<Category>(
             currentPage: input.Page,
---
+++ fc30/CodeFlix/tests/CodeFlix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestsDataGenerator.cs
@@ -0,0 +1,33 @@
+using CodeFlix.Catalog.Application.UseCases.Category.ListCategories;
+
+namespace CodeFlix.Catalog.UnitTests.Application.ListCategories;
+
+public class ListCategoriesTestsDataGenerator
+{
+    public static IEnumerable<object[]> GetInputsWithoutAllParameters(int times) {
+        var fixture = new ListCategoriesTestsFixture();
+        var inputBase = fixture.GetValidInput();
+        for (int i = 0; i < times; i++) {
+            switch (i % 6) {
+                case 0:
+                    yield return new object[] { new ListCategoriesInput() };
+                    break;
+                case 1:
+                    yield return new object[] { new ListCategoriesInput(inputBase.Page) };
+                    break;
+                case 2:
+                    yield return new object[] { new ListCategoriesInput(inputBase.Page, inputBase.PerPage) };
+                    break;
+                case 3:
+                    yield return new object[] { new ListCategoriesInput(inputBase.Page, inputBase.PerPage, inputBase.Search) };
+                    break;
+                case 4:
+                    yield return new object[] { new ListCategoriesInput(inputBase.Page, inputBase.PerPage, inputBase.Search, inputBase.Sort) };
+                    break;
+                case 5:
+                    yield return new object[] { inputBase };
+                    break;
+            }
+        }
+    }
+}
